7b0b8044c4c5a30b270214868ffb9ebfd3049ae8
Emanuel Rivera <riveraaemanuel8418@gmail.com>
2025-08-08 10:37:31 -0500

    Creamos el controlador

---
+++ src/main/java/com/formacionntt/microservicios/app/usuarios/controllers/AlumnoController.java
@@ -0,0 +1,8 @@
+package com.formacionntt.microservicios.app.usuarios.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AlumnoController {
+
+}
