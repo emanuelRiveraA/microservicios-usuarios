6070f2c95388d0a12cd0cf566a316d1d91ff6e89
Emanuel Rivera <riveraaemanuel8418@gmail.com>
2025-08-08 10:33:01 -0500

    Implementamos los metodos e inyectamos el repositorio

--- src/main/java/com/formacionntt/microservicios/app/usuarios/services/AlumnoServiceImpl.java
+++ src/main/java/com/formacionntt/microservicios/app/usuarios/services/AlumnoServiceImpl.java
@@ -2,35 +2,41 @@ package com.formacionntt.microservicios.app.usuarios.services;
 
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.formacionntt.microservicios.app.usuarios.models.entity.Alumno;
+import com.formacionntt.microservicios.app.usuarios.models.repository.AlumnoRepository;
 
 @Service
 public class AlumnoServiceImpl implements AlumnoService {
+	
+	@Autowired
+	private AlumnoRepository alumnoRepository;
 
 	@Override
+	@Transactional(readOnly = true)
 	public Iterable<Alumno> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		return alumnoRepository.findAll();
 	}
 
 	@Override
+	@Transactional(readOnly = true)
 	public Optional<Alumno> findById(Long id) {
-		// TODO Auto-generated method stub
-		return null;
+		return alumnoRepository.findById(id);
 	}
 
 	@Override
+	@Transactional
 	public Alumno save(Alumno alumno) {
-		// TODO Auto-generated method stub
-		return null;
+		return alumnoRepository.save(alumno);
 	}
 
 	@Override
+	@Transactional
 	public void deleteById(Long id) {
-		// TODO Auto-generated method stub
-
+		alumnoRepository.deleteById(id);
 	}
 
 }
