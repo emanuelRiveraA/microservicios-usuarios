79bef607106155dc87d2cc6086eddfbeccaab848
Emanuel Rivera <riveraaemanuel8418@gmail.com>
2025-08-12 18:27:04 -0500

    Se eliminan los metodos y se hereda de common service

--- src/main/java/com/formacionntt/microservicios/app/usuarios/services/AlumnoService.java
+++ src/main/java/com/formacionntt/microservicios/app/usuarios/services/AlumnoService.java
@@ -1,25 +1,10 @@
 package com.formacionntt.microservicios.app.usuarios.services;
 
-import java.util.Optional;
-
 import com.formacionntt.microservicios.app.usuarios.models.entity.Alumno;
+import com.formacionntt.microservicios.commons.services.CommonService;
 
 //Define una interfaz llamada AlumnoService.
-public interface AlumnoService {
+public interface AlumnoService extends CommonService<Alumno> {
 
- // Declara un método para obtener todos los alumnos.
- // Devuelve un Iterable, lo que permite recorrer la colección con un for-each.
- public Iterable<Alumno> findAll();
-
- // Declara un método para buscar un alumno por su ID.
- // Devuelve un Optional<Alumno>, que puede contener un alumno o estar vacío si no se encuentra.
- public Optional<Alumno> findById(Long id);
-
- // Declara un método para guardar un alumno (crear o actualizar).
- // Recibe un objeto Alumno y devuelve el mismo objeto ya persistido.
- public Alumno save(Alumno alumno);
-
- // Declara un método para eliminar un alumno por su ID.
- // No devuelve nada (void).
- public void deleteById(Long id);
+ 
 }
