49da1180dc6d0f526adfa6bafae1d3131c4d8ad6
Emanuel Rivera <riveraaemanuel8418@gmail.com>
2025-08-14 00:29:31 -0500

    se cambio import de Entity Alumno en el repository

--- src/main/java/com/formacionntt/microservicios/app/usuarios/models/repository/AlumnoRepository.java
+++ src/main/java/com/formacionntt/microservicios/app/usuarios/models/repository/AlumnoRepository.java
@@ -2,7 +2,7 @@ package com.formacionntt.microservicios.app.usuarios.models.repository;
 
 import org.springframework.data.repository.CrudRepository;
 
-import com.formacionntt.microservicios.app.usuarios.models.entity.Alumno;
+import com.formacionntt.microservicios.commons.alumnos.models.entity.Alumno;
 
 public interface AlumnoRepository extends CrudRepository<Alumno, Long> {
 
