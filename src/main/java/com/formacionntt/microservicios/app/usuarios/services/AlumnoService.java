b9732d3f85452b222000067a4c78315275892b4d
Emanuel Rivera <riveraaemanuel8418@gmail.com>
2025-08-14 00:29:47 -0500

    se cambio import de Entity Alumno en el service

--- src/main/java/com/formacionntt/microservicios/app/usuarios/services/AlumnoService.java
+++ src/main/java/com/formacionntt/microservicios/app/usuarios/services/AlumnoService.java
@@ -1,6 +1,6 @@
 package com.formacionntt.microservicios.app.usuarios.services;
 
-import com.formacionntt.microservicios.app.usuarios.models.entity.Alumno;
+import com.formacionntt.microservicios.commons.alumnos.models.entity.Alumno;
 import com.formacionntt.microservicios.commons.services.CommonService;
 
 //Define una interfaz llamada AlumnoService.
