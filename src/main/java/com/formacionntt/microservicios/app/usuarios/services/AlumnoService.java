c33434849347d4db77847747f743426c932a7fe6
Emanuel Rivera <riveraaemanuel8418@gmail.com>
2025-08-15 18:28:02 -0500

    Se agrega metodo especializado en servicio

--- src/main/java/com/formacionntt/microservicios/app/usuarios/services/AlumnoService.java
+++ src/main/java/com/formacionntt/microservicios/app/usuarios/services/AlumnoService.java
@@ -1,10 +1,13 @@
 package com.formacionntt.microservicios.app.usuarios.services;
 
+import java.util.List;
+
 import com.formacionntt.microservicios.commons.alumnos.models.entity.Alumno;
 import com.formacionntt.microservicios.commons.services.CommonService;
 
 //Define una interfaz llamada AlumnoService.
 public interface AlumnoService extends CommonService<Alumno> {
 
+	public List<Alumno> findByNombreOrApellido(String term);
  
 }
