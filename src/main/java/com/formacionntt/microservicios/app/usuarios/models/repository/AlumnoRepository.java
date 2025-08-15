ab01cc1b9bac2a9fbaf08efee881d2c11ed40fce
Emanuel Rivera <riveraaemanuel8418@gmail.com>
2025-08-15 18:27:48 -0500

    Se agrega metodo especializado en repositorio

--- src/main/java/com/formacionntt/microservicios/app/usuarios/models/repository/AlumnoRepository.java
+++ src/main/java/com/formacionntt/microservicios/app/usuarios/models/repository/AlumnoRepository.java
@@ -1,9 +1,14 @@
 package com.formacionntt.microservicios.app.usuarios.models.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.formacionntt.microservicios.commons.alumnos.models.entity.Alumno;
 
 public interface AlumnoRepository extends CrudRepository<Alumno, Long> {
 
+	@Query("SELECT a FROM Alumno a WHERE a.nombre LIKE %?1% OR a.apellido LIKE %?1%")
+	public List<Alumno> findByNombreOrApellido(String term);
 }
